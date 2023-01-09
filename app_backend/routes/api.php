4f2194ddf0831882e19c31c1ae06940de88560e7
ngaeninurul <nurulngaeni13@student.uns.ac.id>
2023-01-09 08:26:59 +0700

    Update : set up config staging and production

--- app_backend/routes/api.php
+++ app_backend/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', function () {
+    return 'test';
+});
+
+
+Route::get('/test', function () {
+    return 'test';
+});
