5b796898659f3da9cf8d72a5a261c994c07fbc92
ngaeninurul <nurulngaeni13@student.uns.ac.id>
2023-01-10 13:53:02 +0700

    test

--- app_frontend/src/App.test.js
+++ app_frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Movie App/i);
+  const linkElement = screen.getByText(/Movie Apps/i);
   expect(linkElement).toBeInTheDocument();
 });
